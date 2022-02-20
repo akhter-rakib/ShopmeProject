399719480c3c45602f880d51c600b795ce82a307
aktaruzzaman.rakib <aktaruzzaman.rakib@bjitgroup.com>
2022-02-20 13:35:56 +0600

    Use TestEntityManager for retrieve data

--- shopmewepparent/shopmebackend/src/test/java/com/shopme/admin/user/UserRepositoryTest.java
+++ shopmewepparent/shopmebackend/src/test/java/com/shopme/admin/user/UserRepositoryTest.java
@@ -1,11 +1,15 @@
 package com.shopme.admin.user;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -15,10 +19,15 @@ public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private TestEntityManager entityManager;
+
     @Test
     public void testCreateUser() {
-        User user = new User("rakibccjt@gmail.com", "Admin123", "rakib", "khan");
-        user.addRole(null);
-        userRepository.save(user);
+        Role roleAdmin = entityManager.find(Role.class, 1);
+        User user = new User("ra@gmail.com", "Admin123", "rakib", "khan");
+        user.addRole(roleAdmin);
+        User savedUser = userRepository.save(user);
+        assertThat(savedUser.getId()).isGreaterThan(0);
     }
 }
