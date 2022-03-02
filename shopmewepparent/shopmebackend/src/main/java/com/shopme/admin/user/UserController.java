dc5179014fc5aa1bfcbf943a69a51f7b944ded3a
aktaruzzaman.rakib <aktaruzzaman.rakib@bjitgroup.com>
2022-03-02 21:06:36 +0600

    complete user registration

--- shopmewepparent/shopmebackend/src/main/java/com/shopme/admin/user/UserController.java
+++ shopmewepparent/shopmebackend/src/main/java/com/shopme/admin/user/UserController.java
@@ -1,10 +1,13 @@
 package com.shopme.admin.user;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -18,5 +21,23 @@ public class UserController {
         List<User> userList = userService.userList();
         model.addAttribute("listUsers", userList);
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model) {
+        List<Role> roleList = userService.roleList();
+        User user = new User();
+        model.addAttribute("user", user);
+        model.addAttribute("roleList", roleList);
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
+        user.setEnable(true);
+        userService.saveUser(user);
+        redirectAttributes.addFlashAttribute("message",
+                "The User Has been Saved Successfully");
+        return "redirect:/users";
     }
 }
--- shopmewepparent/shopmebackend/src/main/java/com/shopme/admin/user/UserService.java
+++ shopmewepparent/shopmebackend/src/main/java/com/shopme/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.user;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,18 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     public List<User> userList() {
         return userRepository.findAll();
+    }
+
+    public List<Role> roleList() {
+        return roleRepository.findAll();
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
     }
 }
