9d8a35c529ac00d4c038cc74257bc22be5e27c0a
aktaruzzaman.rakib <aktaruzzaman.rakib@bjitgroup.com>
2022-03-12 23:35:38 +0600

    Code refactor

--- shopmewepparent/shopmebackend/src/main/java/com/shopme/admin/user/UserRestController.java
+++ shopmewepparent/shopmebackend/src/main/java/com/shopme/admin/user/UserRestController.java
@@ -14,8 +14,7 @@ public class UserRestController {
     }
 
     @PostMapping("users/check_email")
-    public String checkDuplicateEmail(@Param("id") Integer id,
-                                      @Param("email") String email) {
+    public String checkDuplicateEmail(@Param("id") Integer id, @Param("email") String email) {
         return userService.isEmailUnique(id, email) ? "OK" : "Duplicated";
     }
 }
